bb20d3e7d68be34d1e9740e910a3f095a7ec4c12
Ado <adoontheway@gmail.com>
2021-10-04 13:10:42 +0800

    redis connect success

--- service/redis.go
+++ service/redis.go
@@ -10,8 +10,8 @@ var ctx = context.Background()
 
 func InitRedis() {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+		Addr:     "localhost:7001",
+		Password: "12345",
 		DB:       0,
 	})
 	err := rdb.Set(ctx, "key", "value", 0).Err()
